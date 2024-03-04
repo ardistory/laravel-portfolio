06c59c22cbdd59dedcae9384875299d7a27f1467
Ardiansyah Putra <putraardiansyah530@gmail.com>
2024-03-04 21:02:16 +0700

    Update: tailwindcss, livewire, vite, base.app-layout

---
+++ resources/views/base/app-layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ardi Putra | {{ $title ?? config('app.name') }}</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-[#222] text-white">
+    {{ $slot }}
+    @vite('resources/js/app.js')
+</body>
+
+</html>
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout title="Home Page">
+    Test
+</x-app-layout>
